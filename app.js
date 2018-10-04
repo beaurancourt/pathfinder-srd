f1a9b3b709a7d4f704e9dcf130f468c00299a615
Beau Shinkle <beaushinkle@gmail.com>
2018-10-04 13:09:09 -0400

    Added Tasks page

--- app.js
+++ app.js
@@ -142,4 +142,21 @@ app.post('/encounter', loggedIn(), (req, res) => {
   res.redirect(encounterInfo.url)
 });
 
+app.get('/tasks', loggedIn(), (req, res) => {
+  const mediums = [11, 13, 14, 15, 16, 18, 19, 21, 22, 23, 24, 25, 26, 28, 29, 30, 32, 33, 34, 35, 36, 38, 39, 41];
+  const hards = [13, 15, 16, 17, 18, 20, 21, 22, 24, 26, 27, 28, 29, 30, 31, 33, 34, 36, 37, 38, 39, 41, 43, 45];
+  const incredibles = [14, 16, 17, 19, 20, 22, 23, 26, 27, 29, 31, 32, 33, 35, 36, 37, 38, 40, 41, 42, 43, 45, 47, 49];
+  const ultimates = [16, 18, 19, 20, 21, 23, 25, 27, 28, 30, 32, 33, 34, 36, 38, 40, 41, 43, 44, 45, 47, 49, 51, 53];
+  res.render('tasks', {'levels': [...Array(24).keys()].map(index => {
+    return {
+      'level': index,
+      'easy': index + 7,
+      'medium': mediums[index],
+      'hard': hards[index],
+      'incredible': incredibles[index],
+      'ultimate': ultimates[index]
+    }
+  })});
+})
+
 app.listen(process.env.PORT || 3000);
