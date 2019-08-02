b887479985c062462df5e1958ad63d8c5d97e532
Beau Shinkle <beaushinkle@gmail.com>
2019-08-02 14:23:35 -0400

    Add regex search to feats

--- public/js/feats.js
+++ public/js/feats.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+  const createRegex = (term) => {
+    return new RegExp('.*' + term + '.*', 'i');
+  };
+
   $("#general-feat-search").on("keyup", function() {
     var value = $(this).val().toLowerCase().trim();
     if (value) {
+      const regex = createRegex(value);
       $("#general-feats > div").filter(function() {
         const text = $(this).text().toLowerCase();
-        $(this).toggle(text.includes(value));
+        $(this).toggle(!!text.match(regex));
       });
       $("#skill-feats > div").filter(function() {
         $(this).toggle(false);
@@ -22,9 +27,10 @@ $(document).ready(function(){
   $("#skill-feat-search").on("keyup", function() {
     var value = $(this).val().toLowerCase().trim();
     if (value) {
+      const regex = createRegex(value);
       $("#skill-feats > div").filter(function() {
         const text = $(this).text().toLowerCase();
-        $(this).toggle(text.includes(value));
+        $(this).toggle(!!text.match(regex));
       });
       $("#general-feats > div").filter(function() {
         $(this).toggle(false);
