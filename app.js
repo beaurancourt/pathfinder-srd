f66cc501be0e47d39baae2c0fe24410d5f86ed53
Beau Shinkle <beaushinkle@gmail.com>
2019-08-02 13:45:29 -0400

    Separate skill and general feats

--- app.js
+++ app.js
@@ -124,7 +124,16 @@ client.connect((err) => {
 
   app.get('/feats', (req, res) => {
     featTable.find().toArray((err, feats) => {
-      res.render('feats', {'feats': feats})
+      let generalFeats = [];
+      let skillFeats = [];
+      feats.forEach(feat => {
+        if (feat.traits.includes('Skill')) {
+          skillFeats.push(feat);
+        } else {
+          generalFeats.push(feat);
+        }
+      })
+      res.render('feats', {generalFeats, skillFeats})
     })
   })
   app.get('/feats/:featName', (req, res) => {
@@ -169,18 +178,18 @@ client.connect((err) => {
   });
 
   app.get('/tasks', (req, res) => {
-    const mediums = [11, 13, 14, 15, 16, 18, 19, 21, 22, 23, 24, 25, 26, 28, 29, 30, 32, 33, 34, 35, 36, 38, 39, 41];
-    const hards = [13, 15, 16, 17, 18, 20, 21, 22, 24, 26, 27, 28, 29, 30, 31, 33, 34, 36, 37, 38, 39, 41, 43, 45];
-    const incredibles = [14, 16, 17, 19, 20, 22, 23, 26, 27, 29, 31, 32, 33, 35, 36, 37, 38, 40, 41, 42, 43, 45, 47, 49];
-    const ultimates = [16, 18, 19, 20, 21, 23, 25, 27, 28, 30, 32, 33, 34, 36, 38, 40, 41, 43, 44, 45, 47, 49, 51, 53];
+    const mediums = [14, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40, 42, 44, 46, 48, 50];
     res.render('tasks', {'levels': [...Array(24).keys()].map(index => {
+      const medium = mediums[index];
       return {
         'level': index,
-        'easy': index + 7,
-        'medium': mediums[index],
-        'hard': hards[index],
-        'incredible': incredibles[index],
-        'ultimate': ultimates[index]
+        'ie': medium - 10,
+        've': medium - 5,
+        'easy': medium -2,
+        'medium': medium,
+        'hard': medium + 2,
+        'vh': medium + 5,
+        'ih': medium + 10
       }
     })});
   })
@@ -265,7 +274,7 @@ client.connect((err) => {
       .toArray()
       .then(spells => {
         return (spells || []).map(spell => {
-          return {'display': `${spell.name} - Spell`, 'value': `/spells/${spell.name}`}
+          return {'display': `${spell.name} - ${spell.type}`, 'value': `/spells/${spell.name}`}
         })
       })
 
--- public/js/feats.js
+++ public/js/feats.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
-  $("#search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#feats > div").filter(function() {
-      const text = $(this).text().toLowerCase();
-      $(this).toggle(text.includes(value));
-    });
+  $("#general-feat-search").on("keyup", function() {
+    var value = $(this).val().toLowerCase().trim();
+    if (value) {
+      $("#general-feats > div").filter(function() {
+        const text = $(this).text().toLowerCase();
+        $(this).toggle(text.includes(value));
+      });
+      $("#skill-feats > div").filter(function() {
+        $(this).toggle(false);
+      });
+    } else {
+      $("#general-feats > div").filter(function() {
+        $(this).toggle(true);
+      });
+      $("#skill-feats > div").filter(function() {
+        $(this).toggle(true);
+      });
+    }
+  });
+
+  $("#skill-feat-search").on("keyup", function() {
+    var value = $(this).val().toLowerCase().trim();
+    if (value) {
+      $("#skill-feats > div").filter(function() {
+        const text = $(this).text().toLowerCase();
+        $(this).toggle(text.includes(value));
+      });
+      $("#general-feats > div").filter(function() {
+        $(this).toggle(false);
+      });
+    } else {
+      $("#general-feats > div").filter(function() {
+        $(this).toggle(true);
+      });
+      $("#skill-feats > div").filter(function() {
+        $(this).toggle(true);
+      });
+    }
   });
 });
